02dcb69f20be964ab68da1cba4bf3ba3995b08c2
mattmrss2 <78233113+mattmrss2@users.noreply.github.com>
2021-02-12 19:50:21 -0500

    Final

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,26 @@
 /* Put your javascript in here */
+let width = 200;
+let count = 3;
+let car_list = carousel.querySelector('ul');
+let position = 0
+
+const arrayposr = [30, 670, 1310 ]
+console.log('test1')
+function moveleft(){
+}
+
+buttons.querySelector(".right").onclick = function() {
+    console.log(position)
+    if (position < 2) {
+        car_list.style.marginLeft = -1*position*610 - 610 + "px";
+        position += 1;
+        console.log(car_list.style.marginLeft + position)
+    }
+}
+buttons.querySelector(".left").onclick = function() {
+    if (position > 0){
+        car_list.style.marginLeft = -1*(position-1)*610 + "px";
+        position -= 1;
+        console.log(car_list.style.marginLeft + position)
+    }
+}
